674fd5008e1a8f3c2bc6f24d45a2e629163bd73e
Artem Shkurenko <artem@shkurenko.ru>
2020-10-25 00:14:33 +0300

    add font preload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import Head from "next/head";
 import App from "containers/app";
 import "containers/layout/layout.css";
 
 function MyApp({ Component, pageProps }: any) {
   return (
     <App>
+      <Head>
+        <link rel="preload" href="/fonts/sktr-bold.woff2" as="style" />
+        <link rel="preload" href="/fonts/sktr-regular.woff2" as="style" />
+      </Head>
       <Component {...pageProps} />
     </App>
   );
