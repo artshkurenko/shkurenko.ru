20a20e8f37f5289e765b25fa6873e0f108c334bb
Artem Shkurenko <artem@shkurenko.ru>
2020-10-25 00:01:11 +0300

    add email and fix footer color

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -9,7 +9,12 @@ interface Props {
 function Footer(props: Props) {
   return (
     <div className={cn(props.className, styles.block)}>
-      Основан в 1996 году <span className={styles.version}>вер. 0.0.1</span>
+      <div className={styles.copyright}>
+        © Основан в 1996 году <span className={styles.version}>вер. 0.0.1</span>
+      </div>
+      <div className={styles.email}>
+        <a href="mailto:artem@shkurenko.ru">artem@shkurenko.ru</a>
+      </div>
     </div>
   );
 }
